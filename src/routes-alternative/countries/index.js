68a043c1e690d7f561ed7232aff7c3dc7eecc372
CAPT-Zulu <thebox@outlook.com.au>
2024-05-26 17:04:40 +1000

    Deleted alternative routes as I am happy with the current implementation

--- src/routes-alternative/countries/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router({ mergeParams: true });
-const countriesRoute = require('./countries/countries');
-
-// --------------------- Routes ---------------------
-router.use('/', countriesRoute); // Countries routes
-
-// Export the router
-module.exports = router;
--- src/routes-alternative/me/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express');
-const meRoute = require('./me');
-
-// --------------------- Routes ---------------------
-router.use('/', meRoute);
-
-// Export the router
-module.exports = router;
